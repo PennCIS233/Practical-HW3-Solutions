b8dba2c2964863906a5967c0cc202b6dd8be30d8
Kento Sugama <kentosugama@protonmail.ch>
2022-03-23 15:45:24 -0400

    update local server to pull image from ipfs

--- Step3/nft_website/views.py
+++ Step3/nft_website/views.py
@@ -3,19 +3,32 @@ import requests
 
 from algosdk.v2client import algod
 
+PINATA_GATEWAY = "https://gateway.pinata.cloud/ipfs/"
+
+# TODO fill me out
+ASSET_ID = 79849243
 
 def serve_image(request):
+    def ipfs_address_to_cid(address):
+        return address.replace("ipfs://", "").replace("#arc3", "")
+
     algod_client = algod.AlgodClient(
         algod_token="",
         algod_address="https://testnet-algorand.api.purestake.io/ps2",
         headers={"X-API-Key": "3oFTnjf15h9rHdK3QnCcO18tCSIyL1PIa4JvGCTU"}
     )
-    asset_id = 75519703 
     asset = algod_client.asset_info(asset_id)
-    url = asset['params']['url']
-    print(url)
-    img_response = requests.get(url)
-    return HttpResponse(img_response.content, content_type="image/jpeg")
+    ipfs_metadata_address = asset['params']['url']
+    ipfs_metadata_cid = ipfs_address_to_cid(ipfs_metadata_address)
+    response = requests.get(PINATA_GATEWAY + ipfs_metadata_cid)
+    if response.status_code != requests.codes.ok:
+        return "Could not find IPFS metadata" 
+
+    content_type = response.json()["ipfs_mimetype"]
+    ipfs_image_address = response.json()["image"]
+    ipfs_image_cid = ipfs_address_to_cid(ipfs_image_address)
+    img_response = requests.get(PINATA_GATEWAY + ipfs_image_cid)
+    return HttpResponse(img_response.content, content_type=content_type)
 
 def home_page(request):
     return HttpResponse("<h1>Visit localhost:8000/nft to view your NFT!</h1>")
